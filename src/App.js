4bab4970998a2e410da283548b711e4280580d84
Restyled.io <commits@restyled.io>
2021-09-30 01:18:57 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./css/main.css";
 import "./css/rain.css";
-import { makeStyles } from "@material-ui/core/styles";
+
 import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-    },
-  },
-}));
+                               root : {
+                                 "& > *" : {
+                                   margin : theme.spacing(1),
+                                 },
+                               },
+                             }));
 
 function App() {
   const classes = useStyles();
